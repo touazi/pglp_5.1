bbb3c84d661ed67420e91083273a4e31e6b18fa9
Touazi lylia <Touazi.lylia@gmail.com>
2020-03-26 15:20:29 +0100

    creation du projet maven

---
+++ gl_exo5.1/src/main/java/uvsq/fr/gl_exo5/App.java
@@ -0,0 +1,13 @@
+package uvsq.fr.gl_exo5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
