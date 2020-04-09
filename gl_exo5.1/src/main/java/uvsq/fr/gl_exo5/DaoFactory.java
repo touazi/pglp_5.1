4daa81dadc3d72a47e639744cf5dc92a31729db7
Touazi lylia <Touazi.lylia@gmail.com>
2020-04-09 03:22:31 +0200

    inteface dao et la fabrique pour l'instantain du dao groupe

--- gl_exo5.1/src/main/java/uvsq/fr/gl_exo5/DaoFactory.java
+++ gl_exo5.1/src/main/java/uvsq/fr/gl_exo5/DaoFactory.java
@@ -4,5 +4,8 @@ public class DaoFactory {
 	public static DAO<PERSONNE> getPersonneDAO ( ) {
 		return new PersonneDaO ( ) ;
 		}
+	public static DAO<GroupePersonnel> getGroupeDAO ( ) {
+		return new GroupeDao ( ) ;
+		}
 
 }
---
+++ gl_exo5.1/src/main/java/uvsq/fr/gl_exo5/GroupeDao.java
@@ -0,0 +1,29 @@
+package uvsq.fr.gl_exo5;
+
+public class GroupeDao implements DAO<GroupePersonnel> {
+
+	@Override
+	public GroupePersonnel create(GroupePersonnel obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GroupePersonnel find(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GroupePersonnel update(GroupePersonnel obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(GroupePersonnel obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
