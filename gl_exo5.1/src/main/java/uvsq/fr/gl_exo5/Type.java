f1f55fc314f6eb4ff6dff1196757c3d7000a8053
Touazi lylia <Touazi.lylia@gmail.com>
2020-04-09 19:37:07 +0200

    pom et javadoc

--- gl_exo5.1/src/main/java/uvsq/fr/gl_exo5/Type.java
+++ gl_exo5.1/src/main/java/uvsq/fr/gl_exo5/Type.java
@@ -1,6 +1,19 @@
 package uvsq.fr.gl_exo5;
-
+/**
+ * <b>"les type des télephone.</b>
+ * Il existe trois types de numéro de telephone :
+ * <ul>
+ * <li>fix_perso</li>
+ * <li>fix_pro</li>
+ * <li>portable</li>
+ * </ul>
+ * @author TOUAZI,Lylia
+ */
 public enum Type {
+/**
+     * L'énumération des télephones.
+     * @since 2020
+     */
 fix_perso,
 fix_pro,
 portable;
--- gl_exo5.1/src/main/java/uvsq/fr/gl_exo5/fonction.java
+++ gl_exo5.1/src/main/java/uvsq/fr/gl_exo5/fonction.java
@@ -1,7 +1,22 @@
 package uvsq.fr.gl_exo5;
-
+/**
+ * <b>"les type des fonctions exercer.</b>
+ *
+ * j'ai lister 4 fonction qu'une personne peut exercer :
+ * <ul>
+ * <li>directeur</li>
+ * <li>charger_de_mission</li>
+ * <li>emplyer</li>
+ * <li>vendeur</li>
+ * </ul>
+ *
+ * @author TOUAZI,Lylia
+ */
 public enum fonction {
-
+/**
+     * L'énumération des fonctions.
+     * @since 2020
+     */
  directeur,
  charger_de_mission,
  emplyer,
